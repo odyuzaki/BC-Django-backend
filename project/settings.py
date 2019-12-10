d94aa460b8d652a0e2937695e888b232340feaf1
Odyuzaki <odyuzaki@gmail.com>
2019-12-10 12:57:19 +0700

    dockerize django 2

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rlqfw41#an707o%-p0)u&dot4^j3h63457fcwlc6hokjwfiw3+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoapp', 'localhost']
 
 
 # Application definition
