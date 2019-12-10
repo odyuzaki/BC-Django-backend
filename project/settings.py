e8df818921ee9db586a8feaf3a767cdd341805e7
Odyuzaki <odyuzaki@gmail.com>
2019-12-10 13:27:10 +0700

    dockerize django 2 add ip droplet

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rlqfw41#an707o%-p0)u&dot4^j3h63457fcwlc6hokjwfiw3+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoapp', 'localhost']
+ALLOWED_HOSTS = ['djangoapp', 'localhost','142.93.187.172']
 
 
 # Application definition
